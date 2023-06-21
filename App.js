2bae6fbf764f723897adb0fa84420d4047a137bd
shovon487 <mislam6789@gmail.com>
2023-06-21 12:48:02 +0600

    list added

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Project1 from './src/Project1'
+import Project2 from './src/Project2'
 
 export default function App() {
   return (
-    <Project1 />
+    <Project2 />
   )
 }
---
+++ src/Project2.js
@@ -0,0 +1,60 @@
+import { FlatList, Image, StyleSheet } from "react-native";
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Text,
+  Right,
+  Left,
+  Body,
+  Button,
+  Spinner,
+} from "native-base";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Project2() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then(function (response) {
+      setProducts(response.data.products);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Content style={{ paddingTop: 50, marginBottom: 10 }} padder>
+        {products ? (
+          <FlatList
+            data={products}
+            renderItem={({ item }) => {
+              return (
+                <Card>
+                  <CardItem>
+                    <Left>
+                      <Body>
+                        <Text>{item.title}</Text>
+                      </Body>
+                    </Left>
+                    <Right>
+                      <Body>
+                        <Button rounded small>
+                          <Text onPress={() => alert(item.description)}>Details</Text>
+                        </Button>
+                      </Body>
+                    </Right>
+                  </CardItem>
+                </Card>
+              );
+            }}
+            keyExtractor={(item) => item}
+          />
+        ) : (
+          <Spinner />
+        )}
+      </Content>
+    </Container>
+  );
+}
