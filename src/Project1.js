d60d6b265d53b02cf6b9ff959fc01686a47b2989
shovon487 <mislam6789@gmail.com>
2023-06-21 11:53:07 +0600

    1st Project code added

--- src/Project1.js
+++ src/Project1.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function Project1() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {
+    Image,
+    StyleSheet,
+  } from "react-native";
+  import {
+    Container,
+    Content,
+    Card,
+    CardItem,
+    Text,
+    Grid,
+    Row,
+    Col,
+  } from "native-base";
+  
+  export default function Project1() {
+    return (
+      <Container>
+        <Content style={{ paddingTop: 200 }}>
+          <Card>
+            <CardItem>
+              <Grid>
+                <Row>
+                  <Col size={35} style={{ padding: 10 }}>
+                    <Image
+                      source={{
+                        uri: "https://www.duir.ac.bd/wp-content/uploads/2016/03/DU-logo-300x168.jpg",
+                      }}
+                      style={{ resizeMode: "contain", flex: 1 }}
+                    />
+                  </Col>
+                  <Col size={65}>
+                    <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                      MD. Ekhlas Uddin
+                    </Text>
+                    <Text style={{ fontSize: 16, fontStyle: "italic" }}>
+                      Programmer
+                    </Text>
+                    <Text style={{ fontSize: 12, fontStyle: "italic" }}>
+                      Information & Communication Technology Cell
+                    </Text>
+  
+                    <Text style={{ paddingTop: 20 }}>University Of Dhaka</Text>
+                    <Text>Phone: 8801744309402</Text>
+                    <Text>Email: ekhlas@du.ac.bd</Text>
+                    <Text>Ext: 4701</Text>
+                  </Col>
+                </Row>
+              </Grid>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
